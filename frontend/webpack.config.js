29a72fb202e9df30f81fcca517633f16db5de119
bibaijin <bibaijin@gmail.com>
2017-09-06 00:18:53 +0800

    uglify js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: ['./src/index.js'],
@@ -18,10 +19,13 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'eslint-loader'
+                loader: [
+                    'babel-loader'
+                ]
             },
             {
                 test: /\.(html|ico)$/,
+                exclude: /node_modules/,
                 use: [{
                     loader: 'file-loader',
                     options: {
@@ -33,6 +37,7 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
+        new UglifyJSPlugin()
     ],
     resolve: {
         alias: {
